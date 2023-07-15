a2ee97e6c547f4fbbf5bfe839e10f3e39fa24324
André Leodoro <aleodoro93@gmail.com>
2023-07-14 23:58:54 -0300

    refatoração do código da pagina home/ alteração de cor da label da pagina cadastro e redimensionamento das imagens do contato/ criação de cliente vip atraves de herança de classe.

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,46 +1,14 @@
-
 const alertPassword = document.getElementById("Passwordvalidade");
 const alertConfPassword = document.getElementById("confirmPassword");
 const btnRegister = document.getElementById("btnRegister");
-const confPassInpt = document.getElementById("confpassword")
-let costummers = [];
+const confPassInpt = document.getElementById("confpassword");
+let customers = [];
 
-const StoredClient = localStorage.getItem("clientes");
-if (StoredClient) {
-  costummers = JSON.parse(StoredClient);
+const storedCustomers = localStorage.getItem("clientes");
+if (storedCustomers) {
+  costummers = JSON.parse(storedCustomers);
 }
 
-
-
-
-/* aqui puxa a api */
-async function fillendress(cep) {
-  try {
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
-
-    if (data.hasOwnProperty("erro")) {
-      throw new Error("CEP não encontrado");
-    }
-    document.getElementById("estate").value = data.uf;
-    document.getElementById("city").value = data.localidade;
-    document.getElementById("district").value = data.bairro;
-    document.getElementById("street").value = data.logradouro;
-  } catch (error) {
-    alert(error.message);
-  }
-}
-/* EventListnner arrow function para validar e preencher o os dados e endereços referente ao CEP */
-const cepInput = document.getElementById("zipcode");
-cepInput.addEventListener("input", (event) => {
-  const cep = event.target.value.replace(/\D/g, "");
-
-  if (cep.length === 8) {
-    fillendress(cep);
-  }
-});
-
-/* AQUI COMEÇA A CRIAÇÃO DO CLIENTE */
 class Cliente {
   constructor(name, email, password, rg, cep, city, estate, street, number, complement) {
     this.id = Cliente.getNextId();
@@ -57,21 +25,55 @@ class Cliente {
   }
 
   static getNextId() {
-    let currentId = localStorage.getItem('lastClientId');
+    let currentId = localStorage.getItem("lastClientId");
     if (!currentId) {
       currentId = 1;
     } else {
       currentId = parseInt(currentId) + 1;
     }
-    localStorage.setItem('lastClientId', currentId);
+    localStorage.setItem("lastClientId", currentId);
     return currentId;
   }
 }
 
+class ClienteVip extends Cliente {
+  constructor(name, email, password, rg, cep, city, estate, street, number, complement) {
+    super(name, email, password, rg, cep, city, estate, street, number, complement);
+    this.isVip = true;
+  }
+}
+
+async function fillAddress(cep) {
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+
+    if (data.hasOwnProperty("erro")) {
+      throw new Error("CEP não encontrado");
+    }
+
+    document.getElementById("estate").value = data.uf;
+    document.getElementById("city").value = data.localidade;
+    document.getElementById("district").value = data.bairro;
+    document.getElementById("street").value = data.logradouro;
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+const cepInput = document.getElementById("zipcode");
+cepInput.addEventListener("input", (event) => {
+  const cep = event.target.value.replace(/\D/g, "");
+
+  if (cep.length === 8) {
+    fillAddress(cep);
+  }
+});
 
 const form = document.getElementById("reg-form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const rg = document.getElementById("rg").value;
@@ -83,54 +85,57 @@ form.addEventListener("submit", function (event) {
   const complement = document.getElementById("complement").value;
   const password = document.getElementById("password").value;
 
-  if (name === "" || email === "" || rg === "" || cep === "" || street === "" || city === "" || estate === "" || number === "" || complement === "" ||
-    password === "") {
-    alert("Prencha todos os campos! SPOK!");
+  if (
+    name === "" || email === "" || rg === "" || cep === "" || street === "" || city === "" || estate === "" || number === "" || complement === "" || password === ""
+  ) { alert("Preencha todos os campos!");
+    return;}
+
+  if (name.length < 3) {
+    alert("Digite um nome válido!");
+    return;
+  }
+ /* o emerson esteve aqui */
+  const matchEmail = customers.find((customer) => customer.email === email);
+  if (matchEmail) {
+    alert("Email já cadastrado, tente novamente!");
     return;
   }
 
-  if (name.length < 3){
-    alert("Digite um nome valido!");
-    return;
-  }
-
-  
-  const matchEmail = costummers.find((costumer)=> costumer.email === email);
-  if(matchEmail){
-  alert("Email ja cadastrado, tente novamente!");
-  return;
-}
-const matchRg = costummers.find((costumer)=> costumer.rg === rg);
-  if(matchRg){
+  const matchRg = customers.find((customer) => customer.rg === rg);
+  if (matchRg) {
     alert("RG já cadastrado, tente novamente!");
-    return
+    return;
   }
 
   if (rg.length < 8 || rg.length > 10) {
-    alert("Digite um RG valido!");
-    return;
-  }
+    alert("Digite um RG válido!");
+    return;}
 
-
-
-  if (password.length < 8) {
-    alertPassword.innerHTML = "A senha deve conter mais de 8 caracteres";
-    alertPassword.style.color = "red";
+  if (password.length < 8) 
+  { alertPassword.innerHTML = "A senha deve conter mais de 8 caracteres";
+    alertPassword.style.color = "#FC6969";
     alertPassword.style.textAlign = "center";
     alertPassword.style.fontSize = "10px";
-    return;
-  } if (confPassInpt.value !== password  ) {
+    return; }
 
-    alertConfPassword.innerHTML = "As senhas não coincidem, tente novamente!";
-    alertConfPassword.style.color = "red";
-    alertConfPassword.style.fontSize = "10px";
-    return;
-  } else {
-    alertPassword.innerHTML = "";
-    alertConfPassword.innerHTML = ""
-  }
-  var costumerscreate = new Cliente(name, email, password, rg, cep, street, city, estate, number, complement);
-  costummers.push(costumerscreate);
-  localStorage.setItem("clientes", JSON.stringify(costummers));
-  form.submit()
+  if(confPassInpt.value !== password)
+   { alertConfPassword.innerHTML = "As senhas não coincidem, tente novamente!";
+    alertConfPassword.style.color = "#FC6969";
+    alertConfPassword.style.fontSize = "10px";  return; }
+    
+    else 
+    { alertPassword.innerHTML = "";
+    alertConfPassword.innerHTML = ""; }
+
+
+  let customer;
+  if (Math.random() < 0.3) 
+  { customer = new ClienteVip(name, email, password, rg, cep, city, estate, street, number, complement);
+    alert("Parabéns! Você foi selecionado como cliente VIP e ganhará um brinde no lançamento!")
+  } else
+   { customer = new Cliente(name, email, password, rg, cep, city, estate, street, number, complement); }
+     costummers.push(customer);
+     localStorage.setItem("clientes", JSON.stringify(costummers));
+  alert("Cadastro realizado com sucesso!")
+  form.submit();
 });
